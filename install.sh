7e0f45021757f16bb6917a2df390481ec9416d27
kbn-gh <kbn.git@gmail.com>
2022-03-23 10:01:29 -0500

    scikit-learn

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
+# conda create -n Sleep_Wake_Scoring python=3.7
 conda install numpy scipy joblib pandas matplotlib ipython opencv seaborn\
               scikit-learn psutil h5py pillow
 
 pip install opencv-python
+pip install -U scikit-learn==0.21.3
+
+# pip install neuraltoolkit
+# pip install musclebeachtools
+~
