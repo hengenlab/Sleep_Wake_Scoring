068270acd338e68e2cd25f6eaa21dd799504459d
kbn-gh <kbn.git@gmail.com>
2022-04-14 16:15:24 -0500

    added ntk and mbt

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ conda install numpy scipy joblib pandas matplotlib ipython opencv seaborn\
 pip install opencv-python
 pip install -U scikit-learn==0.21.3
 
-# pip install neuraltoolkit
-# pip install musclebeachtools
-~
+pip install 'neuraltoolkit@git+https://github.com/hengenlab/neuraltoolkit.git'
+pip install 'musclebeachtools@git+https://github.com/hengenlab/musclebeachtools_hlab.git'
