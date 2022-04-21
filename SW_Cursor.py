efb0e4747ab054ad2616c9d72c838882ab5e2c6c
kbn-gh <kbn.git@gmail.com>
2022-04-21 09:47:46 -0500

    Option to align

--- SW_Cursor.py
+++ SW_Cursor.py
@@ -1,10 +1,10 @@
 import math
-import matplotlib.pyplot as plt
 import numpy as np
+
 
 class Cursor(object):
     def __init__(self, ax1, ax2, ax3):
-        self.clicked=False
+        self.clicked = False
         self.second_click = False
         self.ax1 = ax1
         self.ax2 = ax2
@@ -29,24 +29,25 @@ class Cursor(object):
         # store vlines previously plotted
         self.vl = []
 
-
-        line1 = ax1.plot([0,0], [self.ylims_ax1[0], self.ylims_ax1[1]], linewidth = 0.5, color = 'k')
+        line1 = ax1.plot([0, 0], [self.ylims_ax1[0], self.ylims_ax1[1]],
+                         linewidth=0.5, color='k')
         ml1 = line1.pop(0)
 
-        line2 = ax2.plot([0,0], [self.ylims_ax2[0], self.ylims_ax2[1]], linewidth = 0.5, color = 'k')
+        line2 = ax2.plot([0, 0], [self.ylims_ax2[0], self.ylims_ax2[1]],
+                         linewidth=0.5, color='k')
         ml2 = line2.pop(0)
 
-        line3 = ax3.plot([0,0], [self.ylims_ax3[0], self.ylims_ax3[1]], linewidth = 0.5, color = 'k')
+        line3 = ax3.plot([0, 0], [self.ylims_ax3[0], self.ylims_ax3[1]],
+                         linewidth=0.5, color='k')
         ml3 = line3.pop(0)
 
-        self.movement_x_axis = np.linspace(0,60,900)
-        self.spect_x_axis = np.linspace(199,1442, 900)
+        self.movement_x_axis = np.linspace(0, 60, 900)
+        self.spect_x_axis = np.linspace(199, 1442, 900)
 
         self.lines = [ml1, ml2, ml3]
         self.toggle_line = False
 
         print('making a cursor')
-
 
     def on_move(self, event):
         print('on move')
@@ -55,15 +56,25 @@ class Cursor(object):
         if event.key == 'd':
             print('DONE SCORING')
             self.DONE = True
-        elif event.key in [1,2,3,4]:
+        elif event.key in [1, 2, 3, 4]:
             self.STATE.append(event.key)
         elif event.key == 'l':
             print(f'toggling line!! xdata: {event.xdata} ydata: {event.ydata}')
             for line in self.lines:
                 line.remove()
-            line1 = self.ax1.plot([self.spect_x_axis[int(event.xdata)],self.spect_x_axis[int(event.xdata)]], [self.ylims_ax1[0], self.ylims_ax1[1]], linewidth = 0.5, color = 'k')
-            line2 = self.ax2.plot([int(event.xdata), int(event.xdata)], [self.ylims_ax2[0], self.ylims_ax2[1]], linewidth = 0.5, color = 'k')
-            line3 = self.ax3.plot([self.movement_x_axis[int(event.xdata)],self.movement_x_axis[int(event.xdata)]], [self.ylims_ax3[0], self.ylims_ax3[1]], linewidth = 0.5, color = 'k')
+            line1 = self.ax1.plot([self.spect_x_axis[int(event.xdata)],
+                                   self.spect_x_axis[int(event.xdata)]],
+                                  [self.ylims_ax1[0], self.ylims_ax1[1]],
+                                  linewidth=0.5, color='k')
+            line2 = self.ax2.plot([int(event.xdata), int(event.xdata)],
+                                  [self.ylims_ax2[0],
+                                   self.ylims_ax2[1]],
+                                  linewidth=0.5, color='k')
+            line3 = self.ax3.plot([self.movement_x_axis[int(event.xdata)],
+                                   self.movement_x_axis[int(event.xdata)]],
+                                  [self.ylims_ax3[0],
+                                   self.ylims_ax3[1]],
+                                  linewidth=0.5, color='k')
             self.lines[0] = line1.pop(0)
             self.lines[1] = line2.pop(0)
             self.lines[2] = line3.pop(0)
@@ -74,9 +85,9 @@ class Cursor(object):
             print('MOVIE MODE!')
         else:
             self.movie_mode = False
+
     def pull_up_movie(self, event):
         print('gon pull up some movies')
-
 
     def on_click(self, event):
         if self.movie_mode:
@@ -87,7 +98,8 @@ class Cursor(object):
             if event.inaxes != self.ax2:
                 print('please click in the second figure to select bins')
             else:
-                print(F'SECOND CLICK ----  xdata:{event.xdata} x:{event.x} axes: {event.inaxes}')
+                print("SECOND CLICK ----  xdata:{} x:{} axes: {}"
+                      .format(event.xdata, event.x, event.inaxes))
                 self.bins.append(math.floor(event.xdata))
                 self.clicked = False
                 self.change_bins = True
@@ -97,9 +109,11 @@ class Cursor(object):
                 print('Ploting vertical line for alignment')
 
                 # Create a combined transform from ax1 data to ax2 data
-                combinedTransform1to2 = self.ax1.transData + self.ax2.transData.inverted()
+                combinedTransform1to2 = \
+                    self.ax1.transData + self.ax2.transData.inverted()
                 # Create a combined transform from ax1 data to ax3 data
-                combinedTransform1to3 = self.ax1.transData + self.ax3.transData.inverted()
+                combinedTransform1to3 = \
+                    self.ax1.transData + self.ax3.transData.inverted()
 
                 # Everything below in this block is to plot vline across
                 # all axes ending draw vline
@@ -133,12 +147,8 @@ class Cursor(object):
                 self.ax2.figure.canvas.draw_idle()
                 self.ax3.figure.canvas.draw_idle()
 
-
             else:
                 self.bins.append(math.floor(event.xdata))
-                print(f'FIRST CLICK ----- xdata:{event.xdata} x:{event.x} axes: {event.inaxes}')
+                print("FIRST CLICK ----- xdata:{} x:{} axes: {}"
+                      .format(event.xdata, event.x, event.inaxes))
                 self.clicked = True
-
-
-
-
